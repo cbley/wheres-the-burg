3398fb665a575085cf45f0309c33010f9e259cfe
Christopher Bley <chrisbley@gmail.com>
2015-10-05 20:32:13 -0400

    Change publish to wherestheburg.com

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 var gulp = require('gulp');
-var rename = require('gulp-rename');
 var awspublish = require('gulp-awspublish');
-var path = require('path');
-
 
 gulp.task('upload', function upload() {
   var publisher = awspublish.create({
     params: {
-      Bucket: 'cbley.com'
+      Bucket: 'wherestheburg.com'
     }
   });
 
@@ -16,10 +13,6 @@ gulp.task('upload', function upload() {
   };
 
   return gulp.src('dist/**')
-    .pipe(rename(function renamePath(p) {
-      p.dirname = path.join('wheres-the-burg', p.dirname);
-      // path.dirname =  path.dirname + '/wheres-the-burg';
-    }))
     .pipe(awspublish.gzip({ext: ''}))
     .pipe(publisher.publish(headers))
     .pipe(publisher.cache())
